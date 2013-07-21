2473b79a208537fb98e9c9ddbfc0cade4b526d63
michael72 <Michael.Schulte@gmx.org>
2013-07-21 22:05:02 +0200

    1.0.2-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization  := "org.jaylib.scala.config",
-    version       := "1.0.1",
+    version       := "1.0.2-SNAPSHOT",
     scalaVersion  := "2.10.2",
     // Sonatype OSS deployment
     publishTo <<= version { (v: String) =>
