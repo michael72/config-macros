405afd9c9dc0fa2efa7e6ef553e1550cae88d850
Michael Schulte <michael.schulte@gmx.org>
2014-04-24 22:16:28 +0200

    Update Build.scala
    added macro paradise for Scala 2.10

--- project/Build.scala
+++ project/Build.scala
@@ -2,10 +2,15 @@ import sbt._
 import Keys._
 
 object BuildSettings {
+  val paradiseVersion = "2.0.0"
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization  := "org.jaylib.scala.config",
     version       := "1.1.0-SNAPSHOT",
-    scalaVersion  := "2.10.4",
+    scalaVersion        := "2.10.4",
+    crossScalaVersions := Seq("2.10.2", "2.10.3", "2.10.4", "2.11.0"),
+    resolvers += Resolver.sonatypeRepo("snapshots"),
+    resolvers += Resolver.sonatypeRepo("releases"),
+    addCompilerPlugin("org.scalamacros" % "paradise" % paradiseVersion cross CrossVersion.full),
     // Sonatype OSS deployment
     publishTo <<= version { (v: String) =>
       val nexus = "https://oss.sonatype.org/"
@@ -57,13 +62,18 @@ object ConfigMacroBuild extends Build {
 	  libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-actors" % _ % "test"))  ++ Seq(
 	  //libraryDependencies += "org.scalatest" % "scalatest_2.11.0-RC4" % "2.1.3"))
 	  libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.1.3" % "test"))
-
-lazy val configmacros: Project = Project(
+	  
+  lazy val configmacros: Project = Project(
     "ConfigMacros",
     file("macros"),
 
     settings = buildSettings ++ Seq(
-      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _)) 
+      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _),
+	  libraryDependencies ++= (
+        if (scalaVersion.value.startsWith("2.10")) List("org.scalamacros" %% "quasiquotes" % paradiseVersion)
+        else Nil
+      )
+	  ) 
   ) dependsOn(configbase)
 
  lazy val configmacrotests: Project = Project(
