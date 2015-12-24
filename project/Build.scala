198eb89447d3116b1eed301c88c207530c16ab4f
Michael Schulte <michael.schulte@gmx.org>
2015-12-24 16:40:50 +0100

    adaptions for Scala 2.11

--- project/Build.scala
+++ project/Build.scala
@@ -2,15 +2,12 @@ import sbt._
 import Keys._
 
 object BuildSettings {
-  val paradiseVersion = "2.0.0"
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization  := "org.jaylib.scala.config",
-    version       := "1.1.0-SNAPSHOT",
-    scalaVersion        := "2.10.4",
-    crossScalaVersions := Seq("2.10.2", "2.10.3", "2.10.4", "2.11.0"),
-    resolvers += Resolver.sonatypeRepo("snapshots"),
-    resolvers += Resolver.sonatypeRepo("releases"),
-    addCompilerPlugin("org.scalamacros" % "paradise" % paradiseVersion cross CrossVersion.full),
+    version       := "1.1.0",
+    scalaVersion        := "2.11.7",
+    crossScalaVersions := Seq("2.10.2", "2.10.3", "2.10.4", "2.11.0", "2.11.7"),
+	scalacOptions ++= Seq("-deprecation"),
     // Sonatype OSS deployment
     publishTo <<= version { (v: String) =>
       val nexus = "https://oss.sonatype.org/"
@@ -19,12 +16,11 @@ object BuildSettings {
       else
         Some("releases"  at nexus + "service/local/staging/deploy/maven2")
     },
-    //credentials   += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+    credentials   += Credentials(Path.userHome / ".ivy2" / ".credentials"),
 
     publishMavenStyle := true,
     publishArtifact in Test := false,
     pomIncludeRepository := { _ => false } , 
-	//useGpg := true,
 	pomExtra := (
       <scm>
         <url>git@github.com:michael72/config-macros.git</url>
@@ -60,20 +56,14 @@ object ConfigMacroBuild extends Build {
       libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _ % "test")) ++ Seq(
 	  libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-compiler" % _ % "test"))  ++ Seq(
 	  libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-actors" % _ % "test"))  ++ Seq(
-	  //libraryDependencies += "org.scalatest" % "scalatest_2.11.0-RC4" % "2.1.3"))
-	  libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.1.3" % "test"))
+	  libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"))
 	  
   lazy val configmacros: Project = Project(
     "ConfigMacros",
     file("macros"),
 
     settings = buildSettings ++ Seq(
-      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _),
-	  libraryDependencies ++= (
-        if (scalaVersion.value.startsWith("2.10")) List("org.scalamacros" %% "quasiquotes" % paradiseVersion)
-        else Nil
-      )
-	  ) 
+      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _)) 
   ) dependsOn(configbase)
 
  lazy val configmacrotests: Project = Project(
@@ -83,12 +73,7 @@ object ConfigMacroBuild extends Build {
       libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _)) ++ Seq(
 	  libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-compiler" % _ % "test"))  ++ Seq(
 	  libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-actors" % _ % "test"))  ++ Seq(
-	  libraryDependencies += ("org.scalatest" % "scalatest_2.10" % "2.1.3" % "test")) ++ Seq(publishArtifact := false)
+	  libraryDependencies += ("org.scalatest" % "scalatest_2.11" % "2.2.4" % "test")) ++ Seq(publishArtifact := false)
   ) dependsOn(configmacros)
 
-  /* lazy val examples: Project = Project(
-    "examples",
-    file("examples"),
-    settings = buildSettings ++ Seq(publishArtifact := false)
-  ) dependsOn(configmacros) */
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,7 +1,5 @@
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.2.0")
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "4.0.0")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.3")
-
-useGpg := true
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0") 
 
 
